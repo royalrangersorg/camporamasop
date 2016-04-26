8a543473ea09aa87ee44fee81191f4ad0dfb4a92
royalrangers <rob@royalrangers.co>
2016-04-26 15:34:43 -0400

    Update compile.sh

--- build/compile.sh
+++ build/compile.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-#loop through cur dir and convert to html
-find ./ -iname "*.md" -type f -exec sh -c 'pandoc "${0}" -o "${0%.md}.html"' {} \;
+FILE=manifest.txt
 
-pandoc $1 -0 $1.pdf
-wkhtmltopdf --footer-center [page] --header-left 'Pavillion Orientation [doctitle]' --header-spacing='1cm'  --header-right '[page]/[toPage]'  --header-line --margin-top '1cm'  
+rm -rf all.md
 
-##loop through and convert to pdf
-find ./ -iname "*.html" -type f -exec sh -c 'cat /srv/camporamasop/sop/resources/img/style.html | cat - ${0} > /tmp/foo && mv /tmp/foo ${0}' {} \;
-find ./ -iname "*.html" -type f -exec sh -c 'wkhtmltopdf "${0}" "${0%.html}.pdf"' {} \;
+cat $FILE | while read LINE
+
+do
 
 
+cat $LINE >> all.md
+printf "\n" >> all.md
 
-find ./ -iname "*.md" -type f -exec sh -c 'cat ${0} >> all.md | printf "\n" >> all.md ' {} \;^C
+done
+
 pandoc all.md -o all.html
-cat /srv/camporamasop/sop/resources/img/style.html | cat - all.html > /tmp/foo && mv /tmp/foo all.html
+
+cat /srv/camporamasop/sop/resources/img/style.html | cat - all.html > /tmp/foo && rm -rf all.html && mv /tmp/foo all.html
 wkhtmltopdf all.html all.pdf
+rm -rf all.html
+rm -rf all.md
+
+
